443199061d3b1aeb7512d34888c810bfb1767097
Brad Hicks <oconshien@gmail.com>
2021-11-22 23:09:20 -0500

    Create wbh.bat
    double-click me

---
+++ wbh.bat
@@ -0,0 +1,84 @@
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+@echo off
+
+:::
+:::              _ _,---._
+:::           ,-','       `-.___
+:::          /-;'               `._
+:::         /\/          ._   _,'o \
+:::        ( /\       _,--'\,','"`. )
+:::         |\      ,'o     \'    //\
+:::         |      \        /   ,--'""`-.
+:::         :       \_    _/ ,-'         `-._
+:::          \        `--'  /                )
+:::           `.  \`._    ,'     ________,','
+:::             .--`     ,'  ,--` __\___,;'
+:::              \`.,-- ,' ,`_)--'  /`.,'
+:::               \( ;  | | )      (`-/
+:::                 `--'| |)       |-/
+:::                   | | |        | |
+:::                   | | |,.,-.   | |_
+:::                   | `./ /   )---`  )
+:::                  _|  /    ,',   ,-'
+:::                 ,'|_(    /-<._,' |--,
+:::                 |    `--'---.     \/ \
+:::                 |          / \    /\  \
+:::               ,-^---._     |  \  /  \  \
+:::            ,-'        \----'   \/    \--`.
+:::           /            \              \   \
+:::
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
+
+echo.&echo how do I run your stuff?
+timeout 3
+start "" "https://www.memecreator.org/static/images/memes/5159386.jpg"
+echo.&echo.&echo No, but really, is it Python? idk what to do &echo.
+pause
